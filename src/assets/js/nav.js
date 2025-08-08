7c9648bd3e43aaa28348f29b6138b134d5d642cc
Yunus Mumuni <Soccerghana12342@gmail.com>
2025-08-08 12:06:05 -0400

    fixed nav on mobile for new pages

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -4,9 +4,12 @@
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
 const serviceMenu = document.querySelector("#navigation .dropdown");
-const about = document.querySelector('#About\\ Us')
-const contact = document.querySelector('#Contact')
+const pvcMenu = document.querySelector("#navigation .dropdown2");
+const aluminumMenu = document.querySelector("#navigation .dropdown3");
 
+const aluminum = document.querySelector('#Aluminum')
+const about = document.querySelector('#About')
+const contact = document.querySelector('#Contact')
 const screenWidth = window.screen.width;
 
 
@@ -38,6 +41,55 @@ serviceMenu.addEventListener('click', function () {
     } else {
         serviceMenu.setAttribute("aria-expanded", false);
         serviceMenu.classList.remove("open");
+        if (screenWidth < 770) {
+            about.style.display = 'block'
+            contact.style.display = 'block'
+        }
+
+
+
+    }
+})
+
+pvcMenu.addEventListener('click', function () {
+    const isServiceOpen = pvcMenu.classList.contains("open");
+    if (!isServiceOpen) {
+        pvcMenu.setAttribute("aria-expanded", true);
+        pvcMenu.classList.add("open");
+        if (screenWidth < 770) {
+            about.style.display = 'none'
+            contact.style.display = 'none'
+            aluminum.style.display= 'none'
+        }
+
+
+    } else {
+        pvcMenu.setAttribute("aria-expanded", false);
+        pvcMenu.classList.remove("open");
+        if (screenWidth < 770) {
+            about.style.display = 'block'
+            contact.style.display = 'block'
+            aluminum.style.display= 'block'
+        }
+
+
+
+    }
+});
+aluminumMenu.addEventListener('click', function () {
+    const isServiceOpen = aluminumMenu.classList.contains("open");
+    if (!isServiceOpen) {
+        aluminumMenu.setAttribute("aria-expanded", true);
+        aluminumMenu.classList.add("open");
+        if (screenWidth < 770) {
+            about.style.display = 'none'
+            contact.style.display = 'none'
+        }
+
+
+    } else {
+        aluminumMenu.setAttribute("aria-expanded", false);
+        aluminumMenu.classList.remove("open");
         if (screenWidth < 770) {
             about.style.display = 'block'
             contact.style.display = 'block'
